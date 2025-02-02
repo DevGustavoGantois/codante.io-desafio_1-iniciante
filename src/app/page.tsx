1a2b885def8d4915ca7beb3c869c99cb64051357
Gustavo Gantois <gugagantois@gmail.com>
2025-02-01 21:51:11 -0300

    style:Adjusting font-size of title in mobile version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="mt-20 lg:mt-60 overflow-y-hidden">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10">
-        <h2 className="font-bold text-4xl flex items-center justify-center text-center">
+        <h2 className="font-bold text-2xl lg:text-4xl flex items-center justify-center text-center">
           Aplicação desktop para <br /> Listagem de Países.
         </h2>
         <div>
